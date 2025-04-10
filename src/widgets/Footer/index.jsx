191910442a19d7080551b782fa66ae5c748e0477
Swhite <nakid21@naver.com>
2025-04-10 14:03:58 +0900

    footer merge

--- src/widgets/Footer/index.jsx
+++ src/widgets/Footer/index.jsx
@@ -1,20 +1,38 @@
 import { Link } from 'react-router-dom';
 import './Footer.css'
+import logo from '../../app/assets/react.svg'
+import {ArrowUp} from "lucide-react"
 const Footer = () => {
-  return (<>
-    <footer id="footer">
-      <h1>푸터</h1>
-      <div className='footer-container'>
-        <p className='copy'>
-          <span className='text'>Copyright  &copy; {new Date().getFullYear()}</span>
-          <span className='com'>
-            <Link to={"/"}>ORANGE</Link>
-          </span>
-          <span className="text">All rights reserved.</span>
-        </p>
-      </div>
-    </footer>
-  </>)
+
+  const scrolltotop = () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+  return (
+      <>
+          <header className='footer'>
+              <h1>푸터</h1>
+              <div className='footer-container'>
+                  <Link to={"/"}>
+                      <img src={logo} alt="로고" className='logo' />
+                  </Link>
+                  <p className='text'>
+                      <span>서울특별시 강남구 테헤란로 14길 6 남도빌딩</span>
+                      <span> /team: 박찬우 김영진 이예준</span>
+                  </p>
+                  <p className='copy'>
+                      <span className='text'>Copyrightⓒ{new Date().getFullYear()} Media</span>
+                      <span className='com'>
+                          <Link to={"/"}> 4TuneRecords</Link>
+                      </span>
+                      <span className='text'> All rights reserved.</span>
+                  </p>
+                      <button className='scroll-top-btn' onClick={scrolltotop}>
+                          <ArrowUp size={30} color="#333" />
+                      </button>
+              </div>
+          </header>
+      </>)
 }
+
 
 export default Footer;
